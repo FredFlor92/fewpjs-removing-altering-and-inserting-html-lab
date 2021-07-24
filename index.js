a0df610752b33438c1b19f12d1de6e2d3ef58567
freddyflorial@gmail.com <freddyflorial@gmail.com>
2021-07-24 19:07:27 -0400

    commit tree 89a865497eaca77ecfa6fe2f3ecefb8c0be27be7

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const main = document.getElementById("main");
+main.remove();
+
+let newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "Freddy is the champion"
+
+document.querySelector("body").appendChild(newHeader);
